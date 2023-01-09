08641cc8dbc78893b899b3bd72b871d958977e38
namsilver2111 <49718779+namsilver2111@users.noreply.github.com>
2023-01-09 08:14:24 +0700

    first commit

---
+++ ServiceWorker/Program.cs
@@ -0,0 +1,11 @@
+﻿using ServiceWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
